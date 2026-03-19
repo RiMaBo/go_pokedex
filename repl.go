bc18c920b07efaf3dcda187f63fc0c1bed24b48f
RiMaBo <rimabo@gmail.com>
2026-03-19 11:40:08 -0600

    REPL

--- repl.go
+++ repl.go
@@ -1,7 +1,28 @@
 package main
 
-import "strings"
+import (
+	"bufio";
+	"fmt";
+	"os";
+	"strings";
+)
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())
+		if len(input) < 1 {
+			continue
+		}
+
+		fmt.Println("Your command was:", input[0])
+	}
 }
